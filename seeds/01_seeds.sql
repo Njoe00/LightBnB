f64fbaa7ecbcc78909df964e8b5c3abd9140c38e
Njoe00 <nick.james.joe@gmail.com>
2022-12-15 00:42:16 +0000

    creating three inserts for the lightbnb database

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (id, name, email, password)
+VALUES (1, 'user1', 'user@fake.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(2, 'user2', 'user2@fake.com',  '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(3, 'user3', 'user3@fake.com',  '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (1, 'speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 900, 4, 5, 7, 'canada', '222 fake rd', 'edmonton', 'Alberta', '424242', true ),
+(2, 'Vancouver', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 2000, 3, 2, 1, 'canada', '4360 Marine Dr', 'Vancouver', 'BC', 'V7V 1P1', true ),
+(3, 'Richmond', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 500, 1, 3, 2, 'canada', '8180 Granville Ave', 'Richmond', 'BC', 'V6P 4Z4', true );
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 3, 3);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (1, 1, 1, 4, 'message'),
+(2, 2, 2, 4,'message'),
+(3, 2, 3, 4,'message');
